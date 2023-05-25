64f7836d07faaacaebee0744f57786194a52819a
HelenUgalde <86815586+HelenUgalde@users.noreply.github.com>
2023-05-25 02:06:53 -0600

    se agrega el proyecto

---
+++ ESTANDAR/footer.blade.php
@@ -0,0 +1,20 @@
+
+
+<footer id="main-footer">
+	<p>
+		&copy; 2023 <a> Arboles y plantas </a>
+		&nbsp;&nbsp;
+		<a>Contactanos</a>
+		&nbsp;&nbsp;
+		<a>Mision</a>
+		&nbsp;&nbsp;
+		<a>Vision</a>
+
+	</p>
+		
+
+</footer> <!-- / #main-footer -->
+
+	
+</body>
+</html>
---
+++ ESTANDAR/header.blade.php
@@ -0,0 +1,19 @@
+	<header id="main-header" >
+		
+        <a id="logo-header" href="#">
+            <span class="site-name">Arboles y Plantas</span>
+
+        </a> <!-- / #logo-header -->
+
+        <a>
+		
+		</a>
+
+		<nav>
+			<ul>
+				<li><a href="#">Iniciar sesion</a></li>
+			</ul>
+		</nav><!-- / nav -->
+
+
+	</header><!-- / #main-header -->
